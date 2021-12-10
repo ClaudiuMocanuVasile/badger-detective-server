e90afccb89b058cf88796785583bf73238cb152c
Gabriel Cirstea <gabriel_cirstea@protonmail.com>
2021-12-10 22:23:52 +0200

    a little sql querry

--- BadgerDetectiveWeb/publicHTML/views.py
+++ BadgerDetectiveWeb/publicHTML/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import *
+import sqlite3
 # Create your views here.
 
 def index(request):
@@ -10,8 +11,10 @@ def index(request):
     if request.method == 'POST':
         form = ClientForm(request.POST)
         if form.is_valid():
-            link = request.POST.get('link').split("/")[-2]
-
+            code = request.POST.get('link').split("/")[-2]
+            connection = sqlite3.connect("product_prices.db")
+            cursor = connection.cursor()
+            link = cursor.execute("SELECT date, price FROM prices WHERE id = ? ;", (code,)).fetchall()
     else:
         form = ClientForm()
         link = ""
