e821d4fb7b5ab03d507861ffbde9f4bae9e78e0a
Gabriel Cirstea <gabriel_cirstea@protonmail.com>
2021-12-12 20:54:48 +0200

    add some logs

--- shell/exec_script.sh
+++ shell/exec_script.sh
@@ -5,11 +5,22 @@
 script_file="../price_grabber_script.py"
 links="emag_link.txt"
 dir=$(dirname $0)
+log_dir="logs"
 # be sure we are in the write directory
 [ "$dir" ] && cd $dir
 echo $PWD
+echo $(date) > "$log_dir/record_exec.txt"
+
+[ -d "$log_dir" ] || mkdir "$log_dir"
 
 while read line
 do
-	$script_file $line
+	echo "$line"
+	if [ ! $($script_file $line 2> "$log_dir/err.txt") ]
+	then
+		echo "$line" > "$log_dir/last_link.txt"
+		exit 1
+	fi
 done < $links
+
+echo "It should be done. Move the database \"product_prices.db\" where you need it"
