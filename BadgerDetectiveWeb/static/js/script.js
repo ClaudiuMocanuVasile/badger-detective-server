0b236eb60b8f5fb5c497a4c2aca911c03e85d99d
ClaudiuMocanuVasile <klaudiu2011@gmail.com>
2022-01-01 01:54:06 +0200

    fixed firefox issue and optimised client side code

--- BadgerDetectiveWeb/static/js/script.js
+++ BadgerDetectiveWeb/static/js/script.js
@@ -30,50 +30,57 @@ function do_the_graph(data)
     // var data = "{{rows|safe}}"
     if(document.getElementById("output"))
     {
-        data = data.slice(1, data.length - 1)
-        data = data.split(", ")
-
-        string = ""
+        
+        data = data.replaceAll("(", "[").replaceAll(")", "]").replaceAll("'", '"').replaceAll("-", "/")
+        data = JSON.parse(data)
 
         graph_date_as_string = Array()
-        graph_date = Array()
         graph_prices = Array()
 
-        for(let i = 0; i < data.length; i += 2)
+        for(let i = 0; i < data.length; i += 1)
         {
+            data[i].push(data[i][0])
+            graph_date_as_string.push(data[i][2])
+            data[i][0] = data[i][0].split("/")
+            data[i][0] = data[i][0][1] + "/" + data[i][0][0] + "/" + data[i][0][2]
 
-            data[i] = data[i].slice(2, data[i].length - 1)
-            let tmp = data[i].split("-")
-            data[i] = tmp[1] + "-" + tmp[0] + "-" + tmp[2]
-            graph_date_as_string.push(data[i])
-            data[i+1] = data[i+1].slice(0, data[i+1].length - 1)
-            data[i] = new Date(data[i])
-            graph_date.push(data[i])
-            graph_prices.push(parseFloat(data[i+1]))
-            let d = data[i].getDate()
-            let m = data[i].getMonth() + 1
-            let y = data[i].getFullYear()
-            
-            if( i > 0 && i < data.length - 1)
+            if(Number.isInteger(data[i][1]))
             {
-                let tmp = new Date(data[i])
-                tmp.setDate(tmp.getDate() - 1)
-                tmp = new Date(tmp)
-                let d = tmp.getDate()
-                let m = tmp.getMonth() + 1
-                let y = tmp.getFullYear()
-                string += d + "/" + m + "/" + y + ".<br>"
-            }
-            if( i == data.length - 2)
-            {
-                string += data[i+1] + " RON as of " + d + "/" + m + "/" + y + "."
+                data[i][1] = data[i][1].toString() + ".00"
+                graph_prices.push(parseInt(data[i][1]))
             }
             else
-            {
-                string += data[i+1] + " RON from " + d + "/" + m + "/" + y + " to "
-            }
+                {
+                    if(data[i][1].toString().split(".")[1].length == 1)
+                        data[i][1] = data[i][1].toString() + "0"
+                    else
+                        data[i][1] = data[i][1].toString()
+                        
+                    graph_prices.push(parseFloat(data[i][1]))
+                }
         }
+
+        string = ""
+        
+        for(let i = 0; i < data.length - 1; i += 1)
+        {
+            console.log(data[i])   
+            let x = new Date(data[i][0])
+            let y = new Date(data[i + 1][0])
+
+            const diffTime = Math.abs(x - y);
+            const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+            if(days == 1)
+                string += data[i][1] + " RON pe " + data[i][2] + ".<br>"
+            else
+                string += data[i][1] + " RON intre " + data[i][2] + " si " + data[i+1][2] + ".<br>"
+        }
+
+        string += data[data.length - 1][1] + " RON de pe " + data[data.length - 1][2] + ".<br>"
+
         const ctx = document.getElementById('chart').getContext('2d');
+
         const myChart = new Chart(ctx, {
             type: 'line',
             data: {
